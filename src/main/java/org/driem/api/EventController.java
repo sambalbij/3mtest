09b77f6c3bbcabbd6d2203803ec38e95efcb3c06
Jettro Coenradie <jettro@gridshore.nl>
2016-08-09 15:17:49 +0200

    Improved the annotation to make it a real RestController

--- src/main/java/org/driem/api/EventController.java
+++ src/main/java/org/driem/api/EventController.java
@@ -1,14 +1,13 @@
 package org.driem.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/event")
 public class EventController {
 
@@ -20,7 +19,6 @@ public class EventController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
     public List<Event> events() {
         return eventService.findAllEvents();
     }
