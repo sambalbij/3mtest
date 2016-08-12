ab4578098fb5d090c9eb449155d1ef497e99585d
Jettro Coenradie <jettro@gridshore.nl>
2016-08-12 15:32:46 +0200

    Added two constraints and the buyer_id

--- sql/create_schema.sql
+++ sql/create_schema.sql
@@ -4,38 +4,37 @@
 -- ------------------------------------------------------
 -- Server version	5.7.14
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE = @@TIME_ZONE */;
-/*!40103 SET TIME_ZONE = '+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
-/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `activities`
 --
 
 DROP TABLE IF EXISTS `activities`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `activities` (
-  `id`          INT(11)     NOT NULL AUTO_INCREMENT,
-  `name`        VARCHAR(45) NOT NULL,
-  `description` VARCHAR(250)         DEFAULT NULL,
-  `cost`        DECIMAL(10, 2)       DEFAULT '0',
-  `event_id`    INT(11)     NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  `cost` decimal(10,2) DEFAULT '0.00',
+  `event_id` int(11) NOT NULL,
+  `buyer_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `activity_event_fk_idx` (`event_id`),
-  CONSTRAINT `activity_event_fk` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+  KEY `buyer_fk_idx` (`buyer_id`),
+  CONSTRAINT `activity_event_fk` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `buyer_fk` FOREIGN KEY (`buyer_id`) REFERENCES `participants` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,22 +42,17 @@ CREATE TABLE `activities` (
 --
 
 DROP TABLE IF EXISTS `activity_participant`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `activity_participant` (
-  `activity_id`    INT(11) NOT NULL,
-  `participant_id` INT(11) NOT NULL,
+  `activity_id` int(11) NOT NULL,
+  `participant_id` int(11) NOT NULL,
+  UNIQUE KEY `unique_act_part_idx` (`activity_id`,`participant_id`),
   KEY `activity_fk_idx` (`activity_id`),
   KEY `participant_fk_idx` (`participant_id`),
-  CONSTRAINT `activity_fk` FOREIGN KEY (`activity_id`) REFERENCES `activities` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `participant_fk` FOREIGN KEY (`participant_id`) REFERENCES `participants` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+  CONSTRAINT `activity_fk` FOREIGN KEY (`activity_id`) REFERENCES `activities` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `participant_fk` FOREIGN KEY (`participant_id`) REFERENCES `participants` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,18 +60,16 @@ CREATE TABLE `activity_participant` (
 --
 
 DROP TABLE IF EXISTS `events`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `events` (
-  `id`          INT(11)     NOT NULL AUTO_INCREMENT,
-  `name`        VARCHAR(45) NOT NULL,
-  `finished`    BIT(1)      NOT NULL DEFAULT b'0',
-  `description` VARCHAR(250)         DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `finished` bit(1) NOT NULL DEFAULT b'0',
+  `description` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -85,22 +77,18 @@ CREATE TABLE `events` (
 --
 
 DROP TABLE IF EXISTS `items`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `items` (
-  `id`          INT(11)     NOT NULL AUTO_INCREMENT,
-  `name`        VARCHAR(45) NOT NULL,
-  `description` VARCHAR(250)         DEFAULT NULL,
-  `cost`        DECIMAL(10, 2)       DEFAULT '0',
-  `activity_id` INT(11)     NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  `cost` decimal(10,2) DEFAULT '0.00',
+  `activity_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `item_activity_fk_idx` (`activity_id`),
-  CONSTRAINT `item_activity_fk` FOREIGN KEY (`activity_id`) REFERENCES `activities` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+  CONSTRAINT `item_activity_fk` FOREIGN KEY (`activity_id`) REFERENCES `activities` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -108,22 +96,17 @@ CREATE TABLE `items` (
 --
 
 DROP TABLE IF EXISTS `participant_item`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `participant_item` (
-  `participant_id` INT(11) NOT NULL,
-  `item_id`        INT(11) NOT NULL,
+  `participant_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  UNIQUE KEY `unique_part_item_idx` (`participant_id`,`item_id`),
   KEY `participant_fk_idx` (`participant_id`),
   KEY `item_fk_idx` (`item_id`),
-  CONSTRAINT `item_fk` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `participant_cross_item_fk` FOREIGN KEY (`participant_id`) REFERENCES `participants` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+  CONSTRAINT `item_fk` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `participant_cross_item_fk` FOREIGN KEY (`participant_id`) REFERENCES `participants` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -131,29 +114,25 @@ CREATE TABLE `participant_item` (
 --
 
 DROP TABLE IF EXISTS `participants`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `participants` (
-  `id`       INT(11)     NOT NULL AUTO_INCREMENT,
-  `name`     VARCHAR(45) NOT NULL,
-  `event_id` INT(11)     NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `event_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `participant_event_fk_idx` (`event_id`),
-  CONSTRAINT `participant_event_fk` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+  CONSTRAINT `participant_event_fk` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE = @OLD_TIME_ZONE */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE = @OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES = @OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-08-11 11:14:52
+-- Dump completed on 2016-08-12 15:31:55
